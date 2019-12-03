909db5b8ecfcc30f0d21e6da724f1d865becbd00
milekajtez <nilekajtez@gmail.com>
2019-12-03 20:34:32 +0100

    Dodat glavni meni.

--- UserInterface/MainWindow.py
+++ UserInterface/MainWindow.py
@@ -1,20 +1,23 @@
 import os
 import sys
 
-from PyQt5.QtCore import QByteArray, Qt
+from PyQt5.QtCore import QByteArray, Qt, QState
 from PyQt5.QtGui import QIcon, QMovie
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QSizePolicy, QPushButton, QScrollArea
+from PyQt5.QtMultimedia import QSound
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QSizePolicy, QPushButton, QScrollArea, \
+    QCheckBox, QDesktopWidget
+
+sound = QSound("music.wav")
 
 
-class MainWindow(QWidget, QGridLayout):         # definicija kalse
-    def __init__(self, grid):                   # konstruktor
+class MainWindow(QWidget, QGridLayout):
+    def __init__(self, grid):
         super().__init__()
-        initUI(self, grid)                      # metoda koja se poziva inicijalno...prizak pocetnog menija
+        sound.play()
+        initUI(self, grid)
 
 
 def initUI(self, grid):
-
-    # postavljanje dimenzija prozora,naslova i slike ikonice i boje pozadine..inicijalno se posatvlja
     self.setGeometry(200, 200, 700, 700)
     self.setWindowTitle('Pac-Man')
     absolutePath = os.path.dirname(__file__)
@@ -22,40 +25,35 @@ def initUI(self, grid):
     self.setWindowIcon(QIcon(picturePath))
     QWidget.setStyleSheet(self, "background-color:black")
 
-    #svaki put kada predjem na glavnu stranu moram je update-ovati
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
+
     while grid.count() > 0:
         item = grid.takeAt(0)
-
         if not item:
             continue
-
         w = item.widget()
         if w:
             w.deleteLater()
 
-    # funkcija za postavljanje gif animacije
     animation_gif(self, grid)
-
-    # funckija za dodavanje button-a glavnog menija
     add_main_buttons(self, grid)
-
-    # postavljam grid na widget
     self.setLayout(grid)
-
-    #sluzi da nam se prikaze prozor
     self.show()
 
 
 def animation_gif(self, grid):
     movie_screen = QLabel()
     # expand and center the label
-    #movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    # movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     movie_screen.setAlignment(Qt.AlignCenter)
 
     absolutePath = os.path.dirname(__file__)
-    picturePath = absolutePath[0:len(absolutePath)-14:1] + '/Pictures/pacmanGif.gif'
+    picturePath = absolutePath[0:len(absolutePath) - 14:1] + '/Pictures/pacmanGif.gif'
     self.movie = QMovie(picturePath, QByteArray(), self)
-    #self.movie.setCacheMode(QMovie.CacheAll)
+    # self.movie.setCacheMode(QMovie.CacheAll)
     self.movie.setSpeed(100)
     movie_screen.setMovie(self.movie)
 
@@ -64,11 +62,12 @@ def animation_gif(self, grid):
     self.movie.start()
 
 
-
 def add_main_buttons(self, grid):
-    button_style = 'QPushButton {background-color: transaprent; color: red; font: 10pt, Consolas; fon-size:36px; height:48px; width: 120px}'
-    btn_newgame = QPushButton("NEW GAME", self)
-    btn_newgame.setStyleSheet(button_style)
+    button_style = 'QPushButton {background-color: transparent; color: red; font: 10pt, Consoles; fon-size:36px; ' \
+                   'height:48px; width: 120px} '
+    btn_newGame = QPushButton("NEW GAME", self)
+    btn_newGame.setStyleSheet(button_style)
+    btn_newGame.clicked.connect(lambda: newGame_update(self, grid))
 
     btn_options = QPushButton("OPTIONS", self)
     btn_options.setStyleSheet(button_style)
@@ -82,36 +81,57 @@ def add_main_buttons(self, grid):
     btn_quit.setStyleSheet(button_style)
     btn_quit.clicked.connect(QApplication.instance().quit)
 
-    grid.addWidget(btn_newgame, 2, 0, 1, 1)
+    grid.addWidget(btn_newGame, 2, 0, 1, 1)
     grid.addWidget(btn_options, 3, 0, 1, 1)
     grid.addWidget(btn_about, 4, 0, 1, 1)
     grid.addWidget(btn_quit, 5, 0, 1, 1)
 
 
-def about_update(self, grid):
+def newGame_update(self, grid):
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
 
-    # brisanje objekata grid-a
     while grid.count() > 0:
         item = grid.takeAt(0)
-
         if not item:
             continue
-
         w = item.widget()
         if w:
             w.deleteLater()
 
+    btn_back = QPushButton("BACK", self)
+    btn_back.setStyleSheet(
+        'QPushButton {background-color: transparent; color: red; font: 10pt, Consoles; height:48px; width: 120px}')
+    grid.addWidget(btn_back)
+    btn_back.clicked.connect(lambda: initUI(self, grid))
 
-    documenation_file = open('about.txt', 'r')
 
-    label = QLabel();
-    informations = ""
-    for i in documenation_file.readlines():
-        informations += i
+def about_update(self, grid):
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
 
-    label.setText(informations)
+    while grid.count() > 0:
+        item = grid.takeAt(0)
+        if not item:
+            continue
+        w = item.widget()
+        if w:
+            w.deleteLater()
+
+    documentationFile = open('about.txt', 'r')
+
+    label = QLabel()
+    information = ""
+    for i in documentationFile.readlines():
+        information += i
+
+    label.setText(information)
     label.setStyleSheet(
-        'QLabel {background-color: transparent; color: red; font: 10pt, Consolas; height:48px; width: 120px}')
+        'QLabel {background-color: transparent; color: red; font: 10pt, Consoles; height:48px; width: 120px}')
     label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     label.setAlignment(Qt.AlignHCenter)
 
@@ -125,13 +145,17 @@ def about_update(self, grid):
 
     btn_back = QPushButton("BACK", self)
     btn_back.setStyleSheet(
-        'QPushButton {background-color: transparent; color: red; font: 10pt, Consolas; height:48px; width: 120px}')
+        'QPushButton {background-color: transparent; color: red; font: 10pt, Consoles; height:48px; width: 120px}')
     grid.addWidget(btn_back)
     btn_back.clicked.connect(lambda: initUI(self, grid))
 
 
 def options_update(self, grid):
-    # brisanje objekata grid-a
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
+
     while grid.count() > 0:
         item = grid.takeAt(0)
 
@@ -142,30 +166,47 @@ def options_update(self, grid):
         if w:
             w.deleteLater()
 
-    style = 'QPushButton {background-color: transparent; color: red; font: 10pt, Consolas; height:48px; width: 120px}'
+    style_label = 'QLabel {background-color: transparent; color: red; font: 10pt, Consoles; height:48px; width: 120px}'
+    style_button = 'QPushButton {background-color: transparent; color: red; font: 10pt, Consoles;' \
+                   ' height:48px; width: 120px}'
+    style_checkBox = 'QCheckBox {background-color: transparent; color: red; font: 10pt, Consoles;' \
+                     ' height:48px; width: 120px}'
 
-    label_options = QLabel("OPTIONS",self);
-    label_options.setStyleSheet(style)
+    label_options = QLabel("OPTIONS", self)
+    label_options.setStyleSheet(style_label)
     label_options.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     label_options.setAlignment(Qt.AlignHCenter)
     grid.addWidget(label_options)
 
-    label_sound = QLabel("SOUND:",self)
-    label_sound.setStyleSheet(style)
+    '''label_sound = QLabel("SOUND:", self)
+    label_sound.setStyleSheet(style_label)
     label_sound.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    label_sound.setAlignment(Qt.AlignLeft)
-    grid.addWidget(label_sound)
+    label_sound.setAlignment(Qt.AlignLeft)'''
 
+    checkBox = QCheckBox("SOUND", self)
+    checkBox.setStyleSheet(style_checkBox)
+    checkBox.setChecked(True)
+    checkBox.stateChanged.connect(lambda: clickBox(self, checkBox.checkState()))
+
+    grid.addWidget(checkBox)
 
     btn_back = QPushButton("BACK", self)
-    btn_back.setStyleSheet(style)
-    grid.addWidget(btn_back)
+    btn_back.setStyleSheet(style_button)
     btn_back.clicked.connect(lambda: initUI(self, grid))
+    grid.addWidget(btn_back)
+
+    # grid.addWidget(btn_back, 2, 0, 1, 1)
+
+
+def clickBox(self, state):
+    if state == Qt.Checked:
+        sound.play()
+    else:
+        sound.stop()
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)        # inicijalizacija aplikacije..uvek ide
-    grid = QGridLayout()                # pram grid u koji cu da smestam sve objekte(slike,gif,dugmad,labele)
-    interface = MainWindow(grid)        # pravim objekat tipa klase koja sluzi za inicijalizaciju prozora.
-                                        # Kao parametar mi je grid koji ce mi biyti u tom prozoru
-    sys.exit(app.exec_())               # Izlazak iz aplikacije....uvek ide
+    app = QApplication(sys.argv)
+    grid = QGridLayout()
+    interface = MainWindow(grid)
+    sys.exit(app.exec_())
